4247a8a3c3317e585db4830840b428ab020de625
Karl <you@example.com>
2026-06-04 15:05:46 +0800

    adjust pwa

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
   },
 };
 
